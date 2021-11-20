0e44c169ca62573a008cf6e85e3fa909c64eb7e3
Leandro Bertoluzzi <leajb10@gmail.com>
2021-11-20 02:30:17 -0300

    Added error handling for DB query

--- app/index.js
+++ app/index.js
@@ -18,41 +18,38 @@ app.get('/', (req, res) => {
 
 // Get DB data
 app.get('/db',function(req,res){
-   
-   var dbHost = config.DB_HOST;
-   var dbPort = config.DB_PORT;
-   var dbUser = config.DB_USER;
-   var dbPass = config.DB_PASS;
-   var dbName   = config.DB_NAME;
 
-   var connectionOptions = {
-     host: dbHost,
-     port: dbPort,
-     user: dbUser,
-     password: dbPass,
-     database: dbName
-   };
+  var dbHost = config.DB_HOST;
+  var dbPort = config.DB_PORT;
+  var dbUser = config.DB_USER;
+  var dbPass = config.DB_PASS;
+  var dbName   = config.DB_NAME;
 
-   var connection = db.createConnection(connectionOptions);
-   var queryStr = 'SELECT * FROM TESTING_ITEM_TABLE';
-   
-   connection.connect();
-   
-   connection.query(queryStr, function (error, results, fields) {
-     if (error) throw error;
-     
-     /*
-     var responseStr = '';
-     results.forEach(function(data){
-        responseStr += data.ITEM_NAME + ' : ';
-     });
-     */
-     
-     var jsonArray = JSON.parse(JSON.stringify(results));
-     res.json(jsonArray);
-   });
-    
-   connection.end();
+  var connectionOptions = {
+    host: dbHost,
+    port: dbPort,
+    user: dbUser,
+    password: dbPass,
+    database: dbName
+  };
+
+  var connection = db.createConnection(connectionOptions);
+  var queryStr = 'SELECT * FROM TESTING_ITEM_TABLE';
+
+  connection.connect();
+
+  connection.query(queryStr, function (error, results, fields) {
+    var jsonArray;
+    if (error){
+      var jsonArray = JSON.parse(JSON.stringify({ error: "DB table not found" }));
+      res.status(500).json(jsonArray);
+    } else {
+      jsonArray = JSON.parse(JSON.stringify(results));
+      res.status(200).json(jsonArray);
+    }
+  });
+
+  connection.end();
 });
 
 // JSON test
@@ -60,7 +57,7 @@ app.use(express.json());  // The middleware json allows to decode the json reque
     
 app.post('/json', function (req, res) {
   console.log(req.body.name);
-  res.json({ yourName: req.body.name });
+  res.status(200).json({ yourName: req.body.name });
 });
 
 app.listen(PORT, HOST);
