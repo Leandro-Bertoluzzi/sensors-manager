2aee2c5dc2c537cef7c7bdeea42679dcd12e6585
Leandro Bertoluzzi <leajb10@gmail.com>
2021-11-20 01:00:42 -0300

    Added database management

--- app/index.js
+++ app/index.js
@@ -1,16 +1,67 @@
 'use strict';
 
 const express = require('express');
+var db = require('mysql');
 const config = require('./config');
 
 // Constants
 const PORT = config.PORT;
 const HOST = config.HOST;
 
-// App
+// ******************** App ******************** //
+
+// Home
 const app = express();
 app.get('/', (req, res) => {
   res.send('Hello World');
+});
+
+// Get DB data
+var responseStr = "MySQL Data:";
+
+app.get('/db',function(req,res){
+   
+   var dbHost = config.DB_HOST;
+   var dbPort = config.DB_PORT;
+   var dbUser = config.DB_USER;
+   var dbPass = config.DB_PASS;
+   var dbName   = config.DB_NAME;
+
+   var connectionOptions = {
+     host: dbHost,
+     port: dbPort,
+     user: dbUser,
+     password: dbPass,
+     database: dbName
+   };
+
+   console.log('MySQL Connection config:');
+   console.log(connectionOptions);
+
+   var connection = db.createConnection(connectionOptions);
+   var queryStr = 'SELECT * FROM TESTING_ITEM_TABLE';
+   
+   connection.connect();
+ 
+   connection.query(queryStr, function (error, results, fields) {
+     if (error) throw error;
+     
+     responseStr = '';
+
+     results.forEach(function(data){
+        responseStr += data.ITEM_NAME + ' : ';
+        console.log(data);
+     });
+
+     if(responseStr.length == 0)
+        responseStr = 'No records found';
+
+     console.log(responseStr);
+
+     res.status(200).send(responseStr);
+   });
+    
+   connection.end();
 });
 
 app.listen(PORT, HOST);
---
+++ db-test/db-test-seed.sql
@@ -0,0 +1,47 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
+--
+-- Generation Time: Nov 20, 2021 at 00:45 AM
+-- Server version: 5.7.36
+-- PHP Version: 7.2.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_testing`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `TESTING_ITEM_TABLE`
+--
+
+CREATE TABLE `TESTING_ITEM_TABLE` (
+  `ITEM_NAME` varchar(50) NOT NULL,
+  `ITEM_DESC` varchar(100) NOT NULL,
+  `ITEM_ONHAND` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `TESTING_ITEM_TABLE`
+--
+
+INSERT INTO `TESTING_ITEM_TABLE` (`ITEM_NAME`, `ITEM_DESC`, `ITEM_ONHAND`) VALUES
+('TEST-ITEM-1', 'TEST-ITEM-DESC-1', 10),
+('TEST-ITEM-2', 'TEST-ITEM-DESC-2', 20);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
